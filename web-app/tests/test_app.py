83a304dfa8774338bde390a8312f22f0b385d96b
gracehe04 <gh2313@nyu.edu>
2025-11-21 21:00:24 -0500

    formatting

--- web-app/tests/test_app.py
+++ web-app/tests/test_app.py
@@ -9,9 +9,7 @@ from requests.exceptions import RequestException
 
 import pytest  # pylint: disable=import-error
 
-sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-)
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from app import app  # pylint: disable=C0413,E0401
 
