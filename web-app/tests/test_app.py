fe4211af2b773354b43a0fa3bbf7ebdb131dfd29
gracehe04 <gh2313@nyu.edu>
2025-11-21 20:43:45 -0500

    lint errors

--- web-app/tests/test_app.py
+++ web-app/tests/test_app.py
@@ -2,13 +2,13 @@
 
 import os
 import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 import json
 from unittest.mock import patch
+
 from requests.exceptions import RequestException
 import pytest
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from app import app
 
